6f391a15ca5973b25fd93920ca387cfe635590ff
liovalenzuela <112414536+liovalenzuela@users.noreply.github.com>
2022-10-08 21:33:12 -0300

    Exámenes de Biología

---
+++ quiz_11.js
@@ -0,0 +1,127 @@
+function check() {
+	var  c = 0;
+	var q1=document.quiz.question1.value;
+	var q2=document.quiz.question2.value;
+	var q3=document.quiz.question3.value;
+	var q4=document.quiz.question4.value;
+	var q5=document.quiz.question5.value;
+    var q6=document.quiz.question6.value;
+	var q7=document.quiz.question7.value;
+	var q8=document.quiz.question8.value;
+	var q9=document.quiz.question9.value;
+	var q10=document.quiz.question10.value;
+	var q11=document.quiz.question11.value;
+	var q12=document.quiz.question12.value;
+	var q13=document.quiz.question13.value;
+	var q14=document.quiz.question14.value;
+	var q15=document.quiz.question15.value;
+    var q16=document.quiz.question16.value;
+	var q17=document.quiz.question17.value;
+	var q18=document.quiz.question18.value;
+	var q19=document.quiz.question19.value;
+	var q20=document.quiz.question20.value;
+	var q21=document.quiz.question21.value;
+	var q22=document.quiz.question22.value;
+	var q23=document.quiz.question23.value;
+	var q24=document.quiz.question24.value;
+	var q25=document.quiz.question25.value;
+    var q26=document.quiz.question26.value;
+	var q27=document.quiz.question27.value;
+	var q28=document.quiz.question28.value;
+	var q29=document.quiz.question29.value;
+	var q30=document.quiz.question30.value;
+	var q31=document.quiz.question31.value;
+	var q32=document.quiz.question32.value;
+	var q33=document.quiz.question33.value;
+	var q34=document.quiz.question34.value;
+	var q35=document.quiz.question35.value;
+    var q36=document.quiz.question36.value;
+	var q37=document.quiz.question37.value;
+	var q38=document.quiz.question38.value;
+	var q39=document.quiz.question39.value;
+	var q40=document.quiz.question40.value;
+	var q41=document.quiz.question41.value;
+	var q42=document.quiz.question42.value;
+	var q43=document.quiz.question43.value;
+	var q44=document.quiz.question44.value;
+	var q45=document.quiz.question45.value;
+    var q46=document.quiz.question46.value;
+	var q47=document.quiz.question47.value;
+	var q48=document.quiz.question48.value;
+	var q49=document.quiz.question49.value;
+	var q50=document.quiz.question50.value;
+	var q51=document.quiz.question51.value;
+	var q52=document.quiz.question52.value;
+	var q53=document.quiz.question53.value;
+	var q54=document.quiz.question54.value;
+	var q55=document.quiz.question55.value;
+    var q56=document.quiz.question56.value;
+	var q57=document.quiz.question57.value;
+	var q58=document.quiz.question58.value;
+	var q59=document.quiz.question59.value;
+	var q60=document.quiz.question60.value;
+
+	if (q1=="Son células anucleadas") {c++}
+	if (q2=="Es un ADN circular y desnudo") {c++}
+	if (q3=="Son células procariontes sin pared celular, con citoesqueleto y con colesterol en la membrana plasmática") {c++}
+	if (q4=="Son moléculas de ADN extracromosómico que pueden transmitirse por conjugación") {c++}
+	if (q5=="Poseen una doble membrana celular (externa e interna) y una fina pared de peptidoglicanos entre ambas") {c++}	
+	if (q6=="Es el conjunto de reacciones químicas que realiza una célula") {c++}
+	if (q7=="Son reacciones que liberan energía y poseen un ΔG negativo") {c++}
+	if (q8=="Es una moneda energética") {c++}
+	if (q9=="Es un proceso catabólico y exergónico, donde se oxidan y degradan las moléculas combustibles") {c++}
+	if (q10=="Moléculas combustibles que se degradan y oxidan durante la respiración celular") {c++}
+	if (q11=="Son polímeros de aminoácidos") {c++}
+	if (q12=="Son organismos eucariontes con pared celular de quitina") {c++}
+	if (q13=="Por fisión binaria o amitosis") {c++}
+	if (q14=="Son parásitos intracelulares obligados") {c++}
+	if (q15=="Se caracteriza por un profago que permanece inactivo") {c++}
+	if (q16=="Es autótrofa, posee cloroplastos y mitocondrias") {c++}
+	if (q17=="Poseen ADN extranuclear, asociado a organelas semiautónomas") {c++}
+	if (q18=="Son organelas semiautónomas porque poseen ADN circular y desnudo y porque se multiplican por amitosis") {c++}
+	if (q19=="Se realizan, la entrada al ciclo de Krebs, el ciclo de Krebs y la fosforilación oxidativa") {c++}
+	if (q20=="Es un pigmento fotosintético, que se encuentra en la membrana tilacoidal") {c++}
+	if (q21=="Es un retrovirus, envuelto, que posee una alta tasa de mutación y provoca una infección lenta") {c++}
+	if (q22=="Los ácidos grasos son moléculas combustibles anfipáticas") {c++}
+	if (q23=="La glucosa es una aldosa polar") {c++}
+	if (q24=="Son moléculas apolares de reserva de energía") {c++}
+	if (q25=="Entre dos moléculas polares") {c++}
+	if (q26=="Está constituida por una doble membrana y el espacio perinuclear se continúa con la luz del REG") {c++}
+	if (q27=="Es un lípido de función estructural anfipático") {c++}
+	if (q28=="Pueden comportarse como moléculas anfóteras, por lo tanto son excelentes como buffers") {c++}
+	if (q29=="La mioglobina almacena y tansporta oxígeno en forma intracelular, constituyendo una reserva de oxígeno en la célula muscular") {c++}
+	if (q30=="Es una enfermedad hereditaria, caracterizada por una hemoglobina mutada, que lleva a la formación de eritrocitos en forma de hoz") {c++}
+	if (q31=="Es inversamante proporcional a la afinidad de una enzima por su sustrato") {c++}
+	if (q32=="Cambiará a la forma T(tensa), para liberar oxígeno y unir protones") {c++}
+	if (q33=="Es un polihidroxialdehído que forma puentes de hidrógeno con el agua") {c++}
+	if (q34=="Podrá sobrevivir durante un tiempo realizando fermentación láctica") {c++}
+	if (q35=="El ph descenderá por la mayor producción de lactato") {c++}
+	if (q36=="Ocurrirá mediada por transportadores a favor del gradiente de concentración") {c++}
+	if (q37=="Ocurre a favor del gradiente de concentración por canales de K+") {c++}
+	if (q38=="Es un transporte activo secundario, dependiente del gradiente de Na+") {c++}
+	if (q39=="El agua se transporta desde una solución más diluída a otra más concentrada") {c++}
+	if (q40=="Mantiene los gradientes de concentración del Na+ y del K+ a ambos lados de la membrana plasmática") {c++}
+	if (q41=="El transporte activo primario") {c++}
+	if (q42=="Realizan fermentación alcohólica produciendo CO2 y etanol") {c++}
+	if (q43=="Disminuye la fotofosforilación") {c++}
+	if (q44=="Se reducen el NAD+ Y el FAD") {c++}
+	if (q45=="Disminuye la velocidad de la cadena respiratoria y disminuye la fosforilación oxidativa") {c++}
+	if (q46=="Poseen ADN circular y desnudo y ribosomas tipo procarionte") {c++}
+	if (q47=="Aumentan la degradación y oxidación de moléculas combustibles y disminuyen la síntesis de ATP") {c++}
+	if (q48=="A + G = C + T") {c++}
+	if (q49=="Es un antirretroviral utilizado en el tratamiento del HIV, actúa como un inhibidor competitivo de la retrotranscriptasa") {c++}
+	if (q50=="Es una encefalopatía espongiforme de etiología priónica") {c++}
+	if (q51=="Posee una estructura (9+0), conformada por nueve tripletes periféricos y sin microtúbulos centrales") {c++}
+	if (q52=="Los triglicéridos son moléculas apolares") {c++}
+	if (q53=="Favorece la forma R de una enzima alostérica y corre la curva hacia la izquierda") {c++}
+	if (q54=="Una Adenina y una ribosa unidos por un enlace β-N₉") {c++}
+	if (q55=="Es un disacárido constituido por dos β-glucosas unidas por un enlace β-1,4") {c++}
+	if (q56=="Un inhibidor que no se une al sitio activo de la enzima, no modifica el Km de la enzima y no se alcanza la Vmax a elevadas concentraciones de sustrato") {c++}
+	if (q57=="Es una enzima alostérica, inhibida por ATP y activada por AMP") {c++}
+	if (q58=="Se realiza en los lisosomas por medio de enzimas hidrolíticas que se activan a pH ácido") {c++}
+	if (q59=="Es independiente de la concentración de sustrato y aumenta a mayores concentraciones de enzima") {c++}
+	if (q60=="Aumentamos la proporción de acidos grasos insaturados") {c++}
+
+    document.getElementById("resultado-final").innerHTML = c + " preguntas correctas."
+
+}
---
+++ quiz_22.js
@@ -0,0 +1,129 @@
+function check() {
+	var  c = 0;
+	var q1=document.quiz.question1.value;
+	var q2=document.quiz.question2.value;
+	var q3=document.quiz.question3.value;
+	var q4=document.quiz.question4.value;
+	var q5=document.quiz.question5.value;
+    var q6=document.quiz.question6.value;
+	var q7=document.quiz.question7.value;
+	var q8=document.quiz.question8.value;
+	var q9=document.quiz.question9.value;
+	var q10=document.quiz.question10.value;
+	var q11=document.quiz.question11.value;
+	var q12=document.quiz.question12.value;
+	var q13=document.quiz.question13.value;
+	var q14=document.quiz.question14.value;
+	var q15=document.quiz.question15.value;
+    var q16=document.quiz.question16.value;
+	var q17=document.quiz.question17.value;
+	var q18=document.quiz.question18.value;
+	var q19=document.quiz.question19.value;
+	var q20=document.quiz.question20.value;
+	var q21=document.quiz.question21.value;
+	var q22=document.quiz.question22.value;
+	var q23=document.quiz.question23.value;
+	var q24=document.quiz.question24.value;
+	var q25=document.quiz.question25.value;
+    var q26=document.quiz.question26.value;
+	var q27=document.quiz.question27.value;
+	var q28=document.quiz.question28.value;
+	var q29=document.quiz.question29.value;
+	var q30=document.quiz.question30.value;
+	var q31=document.quiz.question31.value;
+	var q32=document.quiz.question32.value;
+	var q33=document.quiz.question33.value;
+	var q34=document.quiz.question34.value;
+	var q35=document.quiz.question35.value;
+    var q36=document.quiz.question36.value;
+	var q37=document.quiz.question37.value;
+	var q38=document.quiz.question38.value;
+	var q39=document.quiz.question39.value;
+	var q40=document.quiz.question40.value;
+	var q41=document.quiz.question41.value;
+	var q42=document.quiz.question42.value;
+	var q43=document.quiz.question43.value;
+	var q44=document.quiz.question44.value;
+	var q45=document.quiz.question45.value;
+    var q46=document.quiz.question46.value;
+	var q47=document.quiz.question47.value;
+	var q48=document.quiz.question48.value;
+	var q49=document.quiz.question49.value;
+	var q50=document.quiz.question50.value;
+	var q51=document.quiz.question51.value;
+	var q52=document.quiz.question52.value;
+	var q53=document.quiz.question53.value;
+	var q54=document.quiz.question54.value;
+	var q55=document.quiz.question55.value;
+    var q56=document.quiz.question56.value;
+	var q57=document.quiz.question57.value;
+	var q58=document.quiz.question58.value;
+	var q59=document.quiz.question59.value;
+	var q60=document.quiz.question60.value;
+	
+// opciones correctas
+
+	if (q1=="Mitocondria") {c++}
+	if (q2=="ATP --> ADP + P") {c++}
+	if (q3=="Posee un ΔG negativo ya que los sustratos poseen más energía que los productos") {c++}
+	if (q4=="Es una enzima de estructura cuaternaria y curva sigmoidea que cambia de forma en presencia del modificador y cuya curva se desplaza a la izquierda") {c++}
+	if (q5=="Pierde su estructura espacial, pero conserva su estructura primaria") {c++}	
+	if (q6=="Es independiente de la luz, pero necesita ATP y NADPH+H sintetizados en la etapa fotodependiente") {c++}
+	if (q7=="Fosfolípidos, ya que las moléculas anfipáticas pueden formar bicapas trilaminares") {c++}
+	if (q8=="Son distintas propiedades de las enzimas de estructura cuaternaria, que hacen referencia a la cooperación entre distintas subunidades y la capacidad de cambiar de forma") {c++}
+	if (q9=="EL PGA, el PGAL y la glucosa") {c++}
+	if (q10=="En el O2(g)") {c++}
+	if (q11=="Las plantas realizan fotosíntesis en los cloroplastos y respiración aeróbica en las mitocondrias") {c++}
+	if (q12=="Inicialmente la marca aparecerá en el PGAL por el ciclo de Calvin y luego en el CO2(g) por la respiración aeróbica") {c++}
+	if (q13=="H2O") {c++}
+	if (q14=="Difusión facilitada - Simporte Na+/gucosa") {c++}
+	if (q15=="El agua actúa como dador de electrones, ya que durante la etapa fotodependiente ocurre la fotólisis del H2O y los electrones son donados al Fotosistema II") {c++}
+	if (q16=="El ácido cáprico producirá más ATP porque es una molécula más reducida que la glucosa y al ser degradado produce más moléculas de acetil-Coa que la glucosa") {c++}
+	if (q17=="El producto final de una vía metabólica, inhibe a la enzima reguladora de esa vía que posee estructura cuaternaria") {c++}
+	if (q18=="Aparecería en los H+, NADPH+H+ y glúcidos") {c++}
+	if (q19=="La síntesis de ATP, a partir del gradiente electroquímico de protones generado por la fotoxidación del agua en la fotosíntesis y por la oxidación de moléculas combustibles en la respiración aeróbica") {c++}
+	if (q20=="38 ATP, 2 ATP") {c++}
+	if (q21=="20 glucosas, 380 glucosas") {c++}
+	if (q22=="Se impedirá la salida de K+ y la célula se despolarizará") {c++}
+	if (q23=="Se une al sitio alostérico, estabiliza la forma T y desplaza la curva a la derecha") {c++}
+	if (q24=="Puente de hidrógeno") {c++}
+	if (q25=="Anfipáticos/Hidrofóbico") {c++}
+	if (q26=="La glucosa es un polihidroxialdehído y existe en forma lineal y cíclica") {c++}
+	if (q27=="La muy alta afinidad con la que se une al grupo hemo, impidiendo la unión del O2(g) a la hemoglobina") {c++}
+	if (q28=="Son moléculas que facilitan el pasaje de H+ a través de la membrana interna mitocondrial, disipando el gradiente electroquímico e inhibiendo la síntesis de ATP") {c++}
+	if (q29=="Una enzima con una temperatura óptima de 37°C, a 20°C  tendrá una menor actividad enzimática") {c++}
+	if (q30=="A menor ph, mayor concentración de H+, por lo tanto las uniones puente de hidrógeno y las uniones iónicas no se formarán correctamente desnaturalizándose la proteína") {c++}
+	if (q31=="El colágeno no desnaturalizado y el colágeno hidrolizado, no tendrán diferencia en cuanto a su supuesta acción") {c++}
+	if (q32=="CO2(g), H2O, GTP y ATP") {c++}
+	if (q33=="Los Anellovirus, son virus desnudos y el ADN de polaridad negativa es complementario al ARNm") {c++}
+	if (q34=="Un predominio de G-C en la composición del ADN para evitar la desnaturalización") {c++}
+	if (q35=="Es una moneda energética que posee dos uniones de alta energía que al hidrolizarse producen un ΔG < 0") {c++}
+	if (q36=="3’-AACGAATTAACGCATTTT-5’") {c++}
+	if (q37=="800 A, 800 G, 3200 T, 3200 C") {c++}
+	if (q38=="Los desmosomas cumplen la función de unión mecánica célula-célula, mientras que los hemidesmosomas cumplen la misma función pero entre la célula y la matriz extracelular") {c++}
+	if (q39=="Poseen un potencial de membrana negativo debido a la salida de K+ a favor de gradiente desde el interior celular") {c++}
+	if (q40=="Provocará la entrada de Na+, produciéndose la despolarización de la membrana (inversión del potencial de membrana)") {c++}
+	if (q41=="Transporte en masa") {c++}
+	if (q42=="La RUBISCO, aumenta la oxidación de la ribulosa 1,5 di-fosfato disminuyendo la síntesis de PGAL") {c++}
+	if (q43=="Aumentar la cantidad de enzima") {c++}
+	if (q44=="La fosforilación") {c++}
+	if (q45=="Alcanzará su Vmax a elevadas concentraciones de sustrato y su Km será mayor") {c++}
+	if (q46=="Aumentando la cantidad de enzima, mediante la inducción génica") {c++}
+	if (q47=="La molécula de ADN se desnaturaliza a mayor temperatura cuanto mayor es su longitud y mayor es su contenido de G-C") {c++}
+	if (q48=="Impedir el pasaje de H2O entre dos células epiteliales y obligar al transporte transcelular de iones y moléculas polares") {c++}
+	if (q49=="Comunican el citoplasma de las células vegetales") {c++}
+	if (q50=="Poseen una estructura (9+2) / Poseen una estructurada formada por microfilamentos") {c++}
+	if (q51=="5’-ATGGCCTTAACGCGTT-3’") {c++}
+	if (q52=="Un ratón, modificado genéticamente y que no produce la PrPc, no puede infectarse al exponerse al PrPsc") {c++}
+	if (q53=="EL pH disminuirá por la mayor producción de ácido láctico y también disminuirá el transporte de Na+/K+, ya que en condiciones anaeróbicas la célula produce menos ATP que en condiciones aeróbicas") {c++}
+	if (q54=="La síntesis de ATP es un proceso endergónico y anabólico, que necesita la energía proveniente de las moléculas combustibles, mientras que la hidrólisis del ATP es un proceso exergónico y catabólico que libera energía para la síntesis de los componentes celulares") {c++}
+	if (q55=="Ribonucleótidos / desoxirribosa y una base nitrogenada") {c++}
+	if (q56=="Los peptidoglicanos son de origen procarionte y están formados por cadenas de N-acetilglucosamina y N-acetilmurámico, unidos a una cadena peptídica. / Son de origen eucarionte y están formados por proteínas y GAG unidos al ácido hialurónico") {c++}
+	if (q57=="Son moléculas de reserva de energía, apolares, que al no atraer H2O, ocupan un volumen menor que los glúcidos") {c++}
+	if (q58=="Las coenzimas oxidadas NAD+ y FAD, se reducen en el ciclo de Krebs") {c++}
+	if (q59=="Se decarboxila la molécula de piruvato, se reduce la coenzima NAD+ y se obtiene Acetil-CoA") {c++}
+	if (q60=="EL NADH+ H+ permite la ganancia de 3 ATPs, por cada NADH+H+ que es oxidado en la cadena respiratoria. Ante el déficit de NAD, disminuye la síntesis de ATP y de allí el cansancio extremo") {c++}
+
+    document.getElementById("resultado-final").innerHTML = c + " preguntas correctas."
+
+}
